97d8c1ab62308aa0a75a4f9c9111defaa972409a
Matthias Hryniszak <padcom@gmail.com>
2012-03-02 21:23:37 +0100

    Initial import

---
+++ src/main/resources/archetype-resources/src/main/java/HomeController.groovy
@@ -0,0 +1,30 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}
+
+import groovy.util.logging.Log4j;
+import groovy.util.logging.Slf4j;
+
+import org.springframework.stereotype.Controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView
+
+@Controller
+@Slf4j
+class HomeController {
+	@RequestMapping("/")
+	ModelAndView index() {
+		log.debug "Processing home page..."
+		def result = new ModelAndView("home/index")
+		result.model.message = "Hello, world! from controller..."
+		return result
+	}
+	
+	@RequestMapping("/test")
+	@ResponseBody String data() {
+		"Hello, world! from some other place"
+	}
+}
---
+++ src/main/resources/archetype-resources/src/main/webapp/WEB-INF/views/home/index.jsp
@@ -0,0 +1,18 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+<!doctype html>
+
+<html>
+
+<head>
+	<title>Home :: Index</title>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script type="text/javascript" src="/${artifactId}/resources/example.js"></script>
+</head>
+
+<body>
+	<h1>${symbol_dollar}{message}</h1>
+</body>
+
+</html>
